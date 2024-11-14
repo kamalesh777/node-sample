f163bcf5ab0c1c1488011d5961aaf1ba46ae43cf
kamalesh777 <ksmaity777@gmail.com>
2024-11-14 21:03:58 +0530

    Fix - index file modified

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
-var router = express.Router();
+var app = express();
 
+app.use(express.json())
 /* GET home page. */
-router.get('/', function(req, res, next) {
+app.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+module.exports = app;
